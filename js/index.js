5340f1daf765893e70310bd75fcaa3757530c2fc
bekboloffisial <tbkg1558@gmail.com>
2022-02-22 17:09:55 +0600

    initial

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$(".movie").slick({
+    slidesToShow: 4,
+    dots: true,
+    centerMode: true,
+    centerPadding: '60px',
+})
+$(".video").slick({
+    slidesToShow: 3,
+    dots: true,
+})
+
+$(".persons").slick({
+    slidesToShow: 4,
+    dots: true,
+})
