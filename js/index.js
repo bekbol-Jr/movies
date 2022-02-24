ae0e2e4a905fb4d734328450535e2cae4927aa21
bekboloffisial <tbkg1558@gmail.com>
2022-02-24 16:39:23 +0600

    responsive

--- js/index.js
+++ js/index.js
@@ -1,15 +1,90 @@
 $(".movie").slick({
+    arrows:true,
     slidesToShow: 4,
     dots: true,
     centerMode: true,
     centerPadding: '60px',
+    responsive: [
+        {
+            breakpoint: 820,
+            settings: {
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 720,
+            settings: {
+                slidesToShow: 2,
+                centerMode: false,
+            }
+        },
+        {
+            breakpoint: 425,
+            settings: {
+                slidesToShow: 1,
+                centerMode: false,
+                arrows: false,
+            }
+        }
+    ]
 })
+
+
+
+
+
 $(".video").slick({
     slidesToShow: 3,
     dots: true,
+    responsive: [
+        {
+            breakpoint: 820,
+            settings: {
+                slidesToShow: 1,
+                centerMode: true,
+                centerPadding: '60px',
+                arrows: false,
+            }
+        },
+        {
+            breakpoint: 570,
+            settings: {
+                slidesToShow: 1,
+                centerMode: false,
+                arrows: false,
+            }
+        }
+    ]
 })
+
+
+
 
 $(".persons").slick({
     slidesToShow: 4,
     dots: true,
+    responsive: [
+        {
+            breakpoint: 820,
+            settings: {
+                slidesToShow: 2,
+                centerMode: true,
+                centerPadding: '60px',
+            }
+        },
+        {
+            breakpoint: 720,
+            settings: {
+                slidesToShow: 2,
+                centerMode: false,
+            }
+        },
+        {
+            breakpoint: 425,
+            settings: {
+                slidesToShow: 1,
+                centerMode: false,
+            }
+        }
+    ]
 })
